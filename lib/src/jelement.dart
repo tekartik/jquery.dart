48b856b00d388f6a1e671b34664e0409d509f668
alex <alex@tekartik.com>
2014-06-20 20:03:50 +0200

    allow creating jQuery element from dom element

--- lib/src/jelement.dart
+++ lib/src/jelement.dart
@@ -3,4 +3,11 @@ part of tekartik_jquery;
 class JElement extends JObjectBase with JObjectWithElement {
   JElement.empty() : super.empty();
   JElement(JsObject jsObject) : super(jsObject);
+
+  factory JElement.fromElement(Element element) {
+    if (element == null) {
+      return null;
+    }
+    return new JElement(jsElement(element));
+  }
 }
