69112d193b3107d393dd6ca2372376401111d096
Alexandre Roux <alex@tekartik.com>
2025-05-21 17:21:32 +0200

    dart 3.8 support

--- lib/src/jobject_base.dart
+++ lib/src/jobject_base.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:js';
 
 import 'package:tekartik_js_utils/js_utils.dart';
--- lib/src/utils_prv.dart
+++ lib/src/utils_prv.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:js';
 
 JsObject? querySelector(String selector) {
